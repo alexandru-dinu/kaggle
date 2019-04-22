a29b1f12f17750bbcd887cce2c462b44d68b5abd
Alexandru Dinu <alex.dinu07@gmail.com>
2019-04-22 19:34:59 +0300

    Add plots from milestone-1.

--- quora/logs/plot.py
+++ quora/logs/plot.py
@@ -2,6 +2,21 @@ import matplotlib.pyplot as plt
 import sys
 
 
+def plot_m1(log_file):
+    train_loss = [float(f.strip().split()[5].split("=")[1]) for f in open(log_file) if "loss" in f]
+    val_loss = [float(f.strip().split()[6].split("=")[1]) for f in open(log_file) if "loss" in f]
+
+    n = len(train_loss)
+
+    plt.plot(range(n), train_loss, 'r-')
+    plt.plot(range(n), val_loss, 'g-')
+    plt.xticks(range(0, n, 2))
+    plt.grid()
+
+    plt.savefig(f'{log_file.split(".")[0]}-plot.png')
+    plt.close()
+
+
 def plot2(log_file):
     loss = [float(f.strip().split()[4]) for f in open(log_file) if "->loss" in f]
 
@@ -39,4 +54,5 @@ def plot(log_file):
     plt.close()
 
 
-plot('m2-v17.log')
+if __name__ == '__main__':
+    plot_m1('m1-2.log')
